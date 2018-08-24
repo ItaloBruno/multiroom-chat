a1ffbc0e8db066a14b9e258ac0f1362fdc9b345e
ItaloBruno <italobruno_207@hotmail.com>
2018-08-23 23:53:36 -0300

    Ajuste em routes, views e controllers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Importação de configurações do servidor
 var application = require('./config/server');
 
-// Colocando o servidor para escutar a porta 80
+// Colocando o servidor para escutar a porta 8080
 application.listen(8080, function(){
-    console.log('Servidor rodando na porta 80');
+    console.log('Servidor rodando na porta 8080');
 });
