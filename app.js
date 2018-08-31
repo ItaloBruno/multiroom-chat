d9f55672245ae384dab4e142e54f0cf48df0ce25
ItaloBruno <italobruno_207@hotmail.com>
2018-08-31 17:42:34 -0300

    Recebendo e validando os dados do formulário de entrada

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var application = require('./config/server');
 
 // Colocando o servidor para escutar a porta 8080
 application.listen(8080, function(){
-    console.log('Servidor rodando na porta 8080');
+    console.log('Servidor online na porta 8080');
 });
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,17 @@
 module.exports.iniciaChat = function(application, req, res){
+    var dadosForm = req.body;
+    console.log(dadosForm);
+
+    req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
+    req.assert('apelido', 'Nome ou apelido deve ter entre 3 e 15 caracteres').len(3,15);
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        console.log(erros);
+        res.render('index', {validacao: erros});
+        return;
+    }
+
     res.render('chat');
 };
